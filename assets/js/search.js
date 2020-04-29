eedcc59562167c21af082bed73d1cb4b962cfd33
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-29 15:50:25 +0000

    Deploy redhat-developer/design-manual to github.com/redhat-developer/design-manual.git:gh-pages

---
+++ assets/js/search.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  var form = document.querySelector("form");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var search = form.querySelector("input[type=search]");
+    search.value = "site:redhat-developer.github.io/design-manual " + search.value;
+    form.submit();
+  });
+});
