844b932778fb932665fca0970090be634172e81e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-29 13:52:15 +0000

    Deploy redhat-developer/design-manual to github.com/redhat-developer/design-manual.git:gh-pages

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,7 +1,9 @@
 // Expand / Collapse Sidenav list items
 $(document).ready(function(){
-  $(".pf-m-expandable").on("click", function() {
-    $(".pf-m-expandable").removeClass("pf-m-expanded");
-    $(this).addClass("pf-m-expanded");
+  $('.rhddx-c-sub-menu a').click(function(){
+    $(this).parent("li").children("ul").slideToggle('active');
+    $(this).attr('aria-expanded', 'true');
+    $(this).toggleClass("rhddx-m-expanded");
   });
 });
+
