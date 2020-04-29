bbfc52bc9331240646bb4ce5ea4bec01ddcb40f2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-29 01:23:19 +0000

    Deploy redhat-developer/design-manual to github.com/redhat-developer/design-manual.git:gh-pages

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,19 +1,6 @@
 // Expand / Collapse Sidenav list items
 $(document).ready(function(){
-  var $subMenu = $('.rhddx-c-sub-menu a');
-  var $thing = $('a.rhddx-c-entry');
-
-  // if ($thing.children('ul li a').hasClass('rhddx-m-active') === true) {
-  //   $('.rhddx-c-sub-menu a').parent('li').children('ul').slideToggle();
-  //   $('a.rhddx-c-sub-menu--item').addClass("rhddx-m-expanded");
-  // }
-
-  // if (($thing).hasClass('rhddx-m-active')) {
-  //   $subMenu.parent("li").children("ul").slideToggle();
-  //   $('a.rhddx-c-sub-menu--item').toggleClass("rhddx-m-expanded");
-  // }
-
-  $subMenu.click(function(){
+  $('.rhddx-c-sub-menu a').click(function(){
     $(this).parent("li").children("ul").slideToggle();
     $(this).attr('aria-expanded', 'true');
     $(this).toggleClass("rhddx-m-expanded");
